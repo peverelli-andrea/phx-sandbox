84fa6778109b9dd4cd53c819b768967df4599b1a
Peverelli Andrea <ndrpeverelli@gmail.com>
2025-09-22 16:22:36 +0200

    +Heading

--- src/public/index.php
+++ src/public/index.php
@@ -9,20 +9,31 @@ use Phx\Atom\Paragraph\Paragraph;
 use Phx\Atom\Paragraph\ParagraphProps;
 use Phx\Atom\Label\Label;
 use Phx\Atom\Label\LabelProps;
-use Phx\Core\Palette;
+use Phx\Atom\Heading\Heading;
+use Phx\Atom\Heading\HeadingProps;
 
+$heading_render = Heading::render(
+	props: new HeadingProps(content: "Heading H1"),
+);
+$heading_html = Bundler::getHtml(render: $heading_render);
 
 $paragraph_render = Paragraph::render(
 	props: new ParagraphProps(content: "Paragraph"),
 );
-$paragraph_html = Bundler::getHtml($paragraph_render);
+$paragraph_html = Bundler::getHtml(render: $paragraph_render);
 
 $label_render = Label::render(
 	props: new LabelProps(content: "Label"),
 );
-$label_html = Bundler::getHtml($label_render);
+$label_html = Bundler::getHtml(render: $label_render);
 
-$bundle = Bundler::make(component_renders: [$paragraph_render, $label_render]);
+$bundle = Bundler::make(
+	component_renders: [
+		$heading_render,
+		$paragraph_render,
+		$label_render,
+	]
+);
 
 $css_bundle = Bundler::getCss(bundle: $bundle);
 $scripts_before_bundle = Bundler::getScriptsBefore(bundle: $bundle);
@@ -38,6 +49,7 @@ echo <<<HTML
 	</head>
 	<body>
 		{$scripts_before_bundle}
+		{$heading_html}
 		{$paragraph_html}
 		{$label_html}
 		{$scripts_after_bundle}
