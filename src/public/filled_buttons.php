6483f3daaf381e6a415d8834d1b8f47a1d1e392b
Peverelli Andrea <ndrpeverelli@gmail.com>
2025-11-24 18:57:48 +0100

    Alpha FilledButton showcase; new colors

--- src/public/filled_buttons.php
+++ src/public/filled_buttons.php
@@ -6,6 +6,7 @@ require_once("../../vendor/autoload.php");
 
 use Phx\Atom\Icon\IconVariant;
 use Phx\Molecule\FilledButton\FilledButton;
+use Phx\Molecule\FilledButton\FilledButtonVariant;
 use Phx\Core\Page;
 
 final class filled_buttons extends Page
@@ -19,6 +20,161 @@ final class filled_buttons extends Page
 			props: [
 				"label" => "Elevated Button",
 				"icon" => IconVariant::ARROW_BACK,
+			],
+			get_ids: true,
+		);
+		$filled_button_elevated_disabled = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"label" => "Disabled Elevated Button",
+				"icon" => IconVariant::ARROW_BACK,
+				"disabled" => true,
+			],
+		);
+		$filled_button_elevated_untoggled = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"label" => "Untoggled Elevated Button",
+				"icon" => IconVariant::ARROW_BACK,
+				"toggleable" => true,
+			],
+		);
+		$filled_button_elevated_toggled = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"label" => "Toggled Elevated Button",
+				"icon" => IconVariant::ARROW_BACK,
+				"toggleable" => true,
+				"toggled" => true,
+			],
+		);
+	
+		$filled_button_filled = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"variant" => FilledButtonVariant::FILLED,
+				"label" => "Filled Button",
+				"icon" => IconVariant::ARROW_BACK,
+			],
+		);
+		$filled_button_filled_disabled = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"variant" => FilledButtonVariant::FILLED,
+				"label" => "Disabled Filled Button",
+				"icon" => IconVariant::ARROW_BACK,
+				"disabled" => true,
+			],
+		);
+		$filled_button_filled_untoggled = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"variant" => FilledButtonVariant::FILLED,
+				"label" => "Untoggled Filled Button",
+				"icon" => IconVariant::ARROW_BACK,
+				"toggleable" => true,
+			],
+		);
+		$filled_button_filled_toggled = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"variant" => FilledButtonVariant::FILLED,
+				"label" => "Toggled Filled Button",
+				"icon" => IconVariant::ARROW_BACK,
+				"toggleable" => true,
+				"toggled" => true,
+			],
+		);
+
+		$filled_button_tonal = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"variant" => FilledButtonVariant::TONAL,
+				"label" => "Tonal Button",
+				"icon" => IconVariant::ARROW_BACK,
+			],
+		);
+		$filled_button_tonal_disabled = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"variant" => FilledButtonVariant::TONAL,
+				"label" => "Disabled Tonal Button",
+				"icon" => IconVariant::ARROW_BACK,
+				"disabled" => true,
+			],
+		);
+		$filled_button_tonal_untoggled = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"variant" => FilledButtonVariant::TONAL,
+				"label" => "Untoggled Tonal Button",
+				"icon" => IconVariant::ARROW_BACK,
+				"toggleable" => true,
+			],
+		);
+		$filled_button_tonal_toggled = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"variant" => FilledButtonVariant::TONAL,
+				"label" => "Toggled Tonal Button",
+				"icon" => IconVariant::ARROW_BACK,
+				"toggleable" => true,
+				"toggled" => true,
+			],
+		);
+
+		$filled_button_outlined = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"variant" => FilledButtonVariant::OUTLINED,
+				"label" => "Outlined Button",
+				"icon" => IconVariant::ARROW_BACK,
+			],
+		);
+		$filled_button_outlined_disabled = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"variant" => FilledButtonVariant::OUTLINED,
+				"label" => "Disabled Outlined Button",
+				"icon" => IconVariant::ARROW_BACK,
+				"disabled" => true,
+			],
+		);
+		$filled_button_outlined_untoggled = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"variant" => FilledButtonVariant::OUTLINED,
+				"label" => "Untoggled Outlined Button",
+				"icon" => IconVariant::ARROW_BACK,
+				"toggleable" => true,
+			],
+		);
+		$filled_button_outlined_toggled = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"variant" => FilledButtonVariant::OUTLINED,
+				"label" => "Toggled Outlined Button",
+				"icon" => IconVariant::ARROW_BACK,
+				"toggleable" => true,
+				"toggled" => true,
+			],
+		);
+
+		$filled_button_text = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"variant" => FilledButtonVariant::TEXT,
+				"label" => "Text Button",
+				"icon" => IconVariant::ARROW_BACK,
+			],
+		);
+		$filled_button_text_disabled = $this->newComponent(
+			component: FilledButton::class,
+			props: [
+				"variant" => FilledButtonVariant::TEXT,
+				"label" => "Disabled Text Button",
+				"icon" => IconVariant::ARROW_BACK,
+				"disabled" => true,
 			],
 		);
 
@@ -39,6 +195,13 @@ final class filled_buttons extends Page
 
 						a { color: white; }
 					}
+
+					.buttons_group {
+						display: flex;
+						flex-direction: row;
+						flex-wrap: true;
+						gap: 16px;
+					}
 				</style>
 				HTML;
 			},
@@ -46,9 +209,61 @@ final class filled_buttons extends Page
 			<a href="index.php">BACK</a>
 			<br />
 			<br />
-			<div style="width:256px">
-				$filled_button_elevated
+				<div class="buttons_group">
+					{$filled_button_elevated->html}
+					<br />
+					$filled_button_elevated_disabled
+					<br />
+					$filled_button_elevated_untoggled
+					<br />
+					$filled_button_elevated_toggled
+				</div>
+				<br />
+				<div class="buttons_group">
+					$filled_button_filled
+					<br />
+					$filled_button_filled_disabled
+					<br />
+					$filled_button_filled_untoggled
+					<br />
+					$filled_button_filled_toggled
+				</div>
+				<br />
+				<div class="buttons_group">
+					$filled_button_tonal
+					<br />
+					$filled_button_tonal_disabled
+					<br />
+					$filled_button_tonal_untoggled
+					<br />
+					$filled_button_tonal_toggled
+				</div>
+				<br />
+				<div class="buttons_group">
+					$filled_button_outlined
+					<br />
+					$filled_button_outlined_disabled
+					<br />
+					$filled_button_outlined_untoggled
+					<br />
+					$filled_button_outlined_toggled
+				</div>
+				<br />
+				<div class="buttons_group">
+					$filled_button_text
+					<br />
+					$filled_button_text_disabled
+				</div>
 			</div>
+			<script>
+				function onClick(id) {
+					console.log(id);
+				}
+
+				document
+					.getElementById("{$filled_button_elevated->ids[0]}")
+					.addEventListener("click", () => onClick("Filled Button Elevated"));
+			</script>
 			HTML,
 		);
 	}
